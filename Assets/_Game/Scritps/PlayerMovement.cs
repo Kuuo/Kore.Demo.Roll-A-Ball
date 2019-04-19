cc7ccdc2b9d49004d57435458a62d55039d004c7
Kuuo <1098335635@qq.com>
2019-04-19 14:35:16 +0800

    first commit

---
+++ Assets/_Game/Scritps/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 8f;
+    public Rigidbody rb;
+
+    private const string InputStrH = "Horizontal";
+    private const string InputStrV = "Vertical";
+
+    private float inputH;
+    private float inputV;
+    private Vector3 force = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        inputH = Input.GetAxis(InputStrH);
+        inputV = Input.GetAxis(InputStrV);
+        
+        force = new Vector3(inputH, 0f, inputV) * speed;
+        rb.AddForce(force);
+    }
+}
---
+++ Assets/_Game/Scritps/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotate = new Vector3(30f, 40f, 50f);
+
+    private void Update()
+    {
+        transform.Rotate(rotate * Time.deltaTime);
+    }
+}
