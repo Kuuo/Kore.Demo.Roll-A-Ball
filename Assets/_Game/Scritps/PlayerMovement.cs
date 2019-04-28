c131d78b127f06efa4bb492ca82138c1bb4d8011
Kuuo <1098335635@qq.com>
2019-04-28 12:16:25 +0800

    init

--- Assets/_Game/Scritps/PlayerMovement.cs
+++ Assets/_Game/Scritps/PlayerMovement.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 8f;
     public Rigidbody rb;
-
+    public float speed = 8f;
+    
+    
     private const string InputStrH = "Horizontal";
     private const string InputStrV = "Vertical";
-
+    
+    
+    private Vector3 force = Vector3.zero;
     private float inputH;
     private float inputV;
-    private Vector3 force = Vector3.zero;
+    
 
     private void FixedUpdate()
     {
         inputH = Input.GetAxis(InputStrH);
         inputV = Input.GetAxis(InputStrV);
-        
+
         force = new Vector3(inputH, 0f, inputV) * speed;
         rb.AddForce(force);
     }
